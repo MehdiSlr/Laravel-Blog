7f4c36e24052451f8be391efa5d454b880423299
Mehdi Salari <mahdi.salari79@yahoo.com>
2024-07-26 18:27:53 +0330

    #11. Use the Filesystem Class to Read a Directory

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,22 +7,31 @@
     <link rel="stylesheet" href="/app.css">
 </head>
 <body>
+<?php foreach ($posts as $post) : ?>
     <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-        
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi rem aut tempore ipsa corrupti amet doloremque sed molestiae facilis harum officia explicabo vel dicta, dolor nemo ea itaque reiciendis quibusdam omnis voluptas nisi culpa temporibus. Totam fuga quas eveniet cupiditate maiores sequi esse cum, labore voluptatem fugit, porro vero sed tempore aperiam dignissimos asperiores voluptates omnis nulla ab similique ipsam voluptate ad aliquid? Amet, saepe vel fuga odio iure, adipisci aspernatur, sapiente voluptatem non alias quidem exercitationem eum. Exercitationem rerum inventore eos suscipit quibusdam aut quos. Tenetur amet molestiae quae exercitationem ipsum eligendi praesentium atque, rem quas sed ab dolor!</p>
-    </article>
+        <?= $post ?>
+{{--        <h1><a href="/posts/my-first-post">My First Post</a></h1>--}}
 
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-        
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi rem aut tempore ipsa corrupti amet doloremque sed molestiae facilis harum officia explicabo vel dicta, dolor nemo ea itaque reiciendis quibusdam omnis voluptas nisi culpa temporibus. Totam fuga quas eveniet cupiditate maiores sequi esse cum, labore voluptatem fugit, porro vero sed tempore aperiam dignissimos asperiores voluptates omnis nulla ab similique ipsam voluptate ad aliquid? Amet, saepe vel fuga odio iure, adipisci aspernatur, sapiente voluptatem non alias quidem exercitationem eum. Exercitationem rerum inventore eos suscipit quibusdam aut quos. Tenetur amet molestiae quae exercitationem ipsum eligendi praesentium atque, rem quas sed ab dolor!</p>
+{{--        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi rem aut tempore ipsa corrupti amet doloremque--}}
+{{--            sed molestiae facilis harum officia explicabo vel dicta, dolor nemo ea itaque reiciendis quibusdam omnis voluptas--}}
+{{--            nisi culpa temporibus. Totam fuga quas eveniet cupiditate maiores sequi esse cum, labore voluptatem fugit, porro vero--}}
+{{--            sed tempore aperiam dignissimos asperiores voluptates omnis nulla ab similique ipsam voluptate ad aliquid? Amet, saepe--}}
+{{--            vel fuga odio iure, adipisci aspernatur, sapiente voluptatem non alias quidem exercitationem eum. Exercitationem rerum--}}
+{{--            inventore eos suscipit quibusdam aut quos. Tenetur amet molestiae quae exercitationem ipsum eligendi praesentium atque,--}}
+{{--            rem quas sed ab dolor!</p>--}}
     </article>
+    <?php endforeach; ?>
 
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-        
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi rem aut tempore ipsa corrupti amet doloremque sed molestiae facilis harum officia explicabo vel dicta, dolor nemo ea itaque reiciendis quibusdam omnis voluptas nisi culpa temporibus. Totam fuga quas eveniet cupiditate maiores sequi esse cum, labore voluptatem fugit, porro vero sed tempore aperiam dignissimos asperiores voluptates omnis nulla ab similique ipsam voluptate ad aliquid? Amet, saepe vel fuga odio iure, adipisci aspernatur, sapiente voluptatem non alias quidem exercitationem eum. Exercitationem rerum inventore eos suscipit quibusdam aut quos. Tenetur amet molestiae quae exercitationem ipsum eligendi praesentium atque, rem quas sed ab dolor!</p>
-    </article>
+{{--    <article>--}}
+{{--        <h1><a href="/posts/my-second-post">My Second Post</a></h1>--}}
+
+{{--        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi rem aut tempore ipsa corrupti amet doloremque sed molestiae facilis harum officia explicabo vel dicta, dolor nemo ea itaque reiciendis quibusdam omnis voluptas nisi culpa temporibus. Totam fuga quas eveniet cupiditate maiores sequi esse cum, labore voluptatem fugit, porro vero sed tempore aperiam dignissimos asperiores voluptates omnis nulla ab similique ipsam voluptate ad aliquid? Amet, saepe vel fuga odio iure, adipisci aspernatur, sapiente voluptatem non alias quidem exercitationem eum. Exercitationem rerum inventore eos suscipit quibusdam aut quos. Tenetur amet molestiae quae exercitationem ipsum eligendi praesentium atque, rem quas sed ab dolor!</p>--}}
+{{--    </article>--}}
+
+{{--    <article>--}}
+{{--        <h1><a href="/posts/my-third-post">My Third Post</a></h1>--}}
+
+{{--        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi rem aut tempore ipsa corrupti amet doloremque sed molestiae facilis harum officia explicabo vel dicta, dolor nemo ea itaque reiciendis quibusdam omnis voluptas nisi culpa temporibus. Totam fuga quas eveniet cupiditate maiores sequi esse cum, labore voluptatem fugit, porro vero sed tempore aperiam dignissimos asperiores voluptates omnis nulla ab similique ipsam voluptate ad aliquid? Amet, saepe vel fuga odio iure, adipisci aspernatur, sapiente voluptatem non alias quidem exercitationem eum. Exercitationem rerum inventore eos suscipit quibusdam aut quos. Tenetur amet molestiae quae exercitationem ipsum eligendi praesentium atque, rem quas sed ab dolor!</p>--}}
+{{--    </article>--}}
 </body>
 </html>
